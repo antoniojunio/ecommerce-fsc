a24ec7af1fd61eb8135c9cc72c6c75b862d1353f
antoniojunio <juniordsj@gmail.com>
2023-07-26 18:15:12 -0300

    feat: add Home Page and React Router Dom

---
+++ src/pages/home/home.page.tsx
@@ -0,0 +1,13 @@
+
+// Components
+import Header from "../../components/header/header.component"
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default HomePage
