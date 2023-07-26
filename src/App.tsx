f04eac121cd70e0a19b93eadaa9957dac9835b6f
antoniojunio <juniordsj@gmail.com>
2023-07-26 17:59:51 -0300

    feat: add Header Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-const App = () => {
-  return <h1>hello</h1>
+import { FunctionComponent } from 'react'
+
+// Components
+import Header from './components/header/header.component'
+
+interface AppProps {
+  message?: string
+}
+
+const App: FunctionComponent<AppProps> = ({ message }) => {
+  return <Header />
 }
 
 export default App
