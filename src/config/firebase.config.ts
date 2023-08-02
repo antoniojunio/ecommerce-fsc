fd1ab05389a55d4b832af6d89a0d3693e8feb778
antoniojunio <juniordsj@gmail.com>
2023-08-01 23:56:06 -0300

    feat: add script that populates firestore db

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   storageBucket: "club-ecommerce-99b6a.appspot.com",
   messagingSenderId: "627795090635",
   appId: "1:627795090635:web:362fd1aaaa59afa3b70f4e"
-};
+}
 
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
