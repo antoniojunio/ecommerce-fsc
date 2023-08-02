2c7666fafd90d20d00eba12dcc4c4e6a04b4154a
antoniojunio <juniordsj@gmail.com>
2023-08-02 07:51:13 -0300

    feat: get categories from firestore db

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,43 +1,50 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../../config/firebase.config'
 
 // Components
 import CategoryItem from '../category-item/category-item.component'
 
 // Utilities
 import Category from '../../types/category.types'
+import { categoryConverter } from '../converters/firestore.converters'
 
 // Styles
 import { CategoriesContainer, CategoriesContent } from './categories.styles'
 
 const Categories = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
-  
-  console.log(import.meta.env);
-  
+  const [categories, setCategories] = useState<Category[]>([])
+
   const fetchCategories = async () => {
     try {
-      const { data } = await axios.get<Category[]>(`${import.meta.env.VITE_APP_API_URL}/api/category`);
-        console.log(data);
-  
-        setCategories(categories);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    useEffect(() => {
-      fetchCategories();
-    }, []);
+      const categoriesFromFirestore: Category[] = []
+
+      const querySnapshot = await getDocs(
+        collection(db, 'categories').withConverter(categoryConverter)
+      )
+
+      querySnapshot.forEach((doc) => {
+        categoriesFromFirestore.push(doc.data())
+      })
+
+      setCategories(categoriesFromFirestore)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
 
   return (
     <CategoriesContainer>
       <CategoriesContent>
-        {categories.map(category => (
+        {categories.map((category) => (
           <div key={category.id}>
             <CategoryItem category={category} />
           </div>
-        ) )}
+        ))}
       </CategoriesContent>
     </CategoriesContainer>
   )
