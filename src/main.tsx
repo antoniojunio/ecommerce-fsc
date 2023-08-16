cd43cf62cc84048431fb114843d9e224a7714cb8
antoniojunio <juniordsj@gmail.com>
2023-08-16 18:09:27 -0300

    feat: add Category Context

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from './App.tsx'
 import './index.css'
 
 import UserContextProvider from './contexts/user.context.tsx'
+import CategoryContextProvider from './contexts/category.context.tsx'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <UserContextProvider>
-      <App />
+      <CategoryContextProvider>
+        <App />
+      </CategoryContextProvider>
     </UserContextProvider>
   </React.StrictMode>
 )
