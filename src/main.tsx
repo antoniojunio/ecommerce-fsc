c78cbc96b1bb9d261ced8e6ef15ac6fbe02f8152
antoniojunio <juniordsj@gmail.com>
2023-08-23 17:58:40 -0300

    chore: add redux setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
 import './index.css'
 
 import UserContextProvider from './contexts/user.context.tsx'
 import CategoryContextProvider from './contexts/category.context.tsx'
 import CartContextProvider from './contexts/cart.context.tsx'
+import store from './store/store.ts'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <UserContextProvider>
-      <CategoryContextProvider>
-        <CartContextProvider>
-          <App />
-        </CartContextProvider>
-      </CategoryContextProvider>
-    </UserContextProvider>
+    <Provider store={store}>
+      <UserContextProvider>
+        <CategoryContextProvider>
+          <CartContextProvider>
+            <App />
+          </CartContextProvider>
+        </CategoryContextProvider>
+      </UserContextProvider>
+    </Provider>
   </React.StrictMode>
 )
