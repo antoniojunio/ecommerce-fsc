e0abcef3f6adb2405de36b4023b8599e4994f98d
antoniojunio <juniordsj@gmail.com>
2023-09-12 23:26:50 -0300

    feat: add redux persist to Cart Reducer

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,24 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.tsx'
 import './index.css'
+// @ts-ignore
+import { PersistGate } from 'redux-persist/integration/react'
 
-// import UserContextProvider from './contexts/user.context.tsx'
 import CategoryContextProvider from './contexts/category.context.tsx'
-import CartContextProvider from './contexts/cart.context.tsx'
-import store from './store/store.ts'
+import {store, persistedStore} from './store/store.ts'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <UserContextProvider> */}
-      <CategoryContextProvider>
-        <CartContextProvider>
+      <PersistGate persistor={persistedStore}>
+        <CategoryContextProvider>
           <App />
-        </CartContextProvider>
-      </CategoryContextProvider>
-      {/* </UserContextProvider> */}
+        </CategoryContextProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
